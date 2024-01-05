709f585361c7dca9cf33eea6e6de9af2b3bef628
kirillmorgunov <kirillmorgunov@mail.ru>
2024-01-05 17:23:09 +0400

    HW1 fix

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -11,24 +11,24 @@ const FriendMessage = (props: MessagePropsType) => {
         >
             <div className={s.friendImageAndText}>
                 <img
-                    id={'hw1-friend-avatar-' + props.message.user.avatar}
+                    id={'hw1-friend-avatar-' + props.message.id}
                     src={props.message.user.avatar}
                 />
                 <div
-                    id={'hw1-friend-time-' + props.message.message.time}
+                    id={'hw1-friend-time-' + props.message.id}
                     className={s.friendTime}
                 >
                     {props.message.message.time}
                 </div>
                 <div className={s.friendText}>
                     <div
-                        id={'hw1-friend-name-' + props.message.user.name}
+                        id={'hw1-friend-name-' + props.message.id}
                         className={s.friendName}
                     >
                         {props.message.user.name}
                     </div>
                     <pre
-                        id={'hw1-friend-text-' + props.message.message.text}
+                        id={'hw1-friend-text-' + props.message.id}
                         className={s.friendMessageText}
                     >
                         {props.message.message.text}
--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -13,17 +13,17 @@ const Message = (props: MessagePropsType) => {
         <div id={'hw1-message-' + props.message.id} className={s.message}>
             <div className={s.imageAndText}>
                 <img
-                    id={'hw1-avatar-' + props.message.user.avatar}
+                    id={'hw1-avatar-' + props.message.id}
                     src={props.message.user.avatar}
                 />
-                <div id={'hw1-time-' + props.message.message.time} className={s.time}>
+                <div id={'hw1-time-' + props.message.id} className={s.time}>
                     {props.message.message.time}
                 </div>
                 <div className={s.text}>
-                    <div id={'hw1-name-' + props.message.user.name} className={s.name}>
+                    <div id={'hw1-name-' + props.message.id} className={s.name}>
                         {props.message.user.name}
                     </div>
-                    <pre id={'hw1-text-' + props.message.message.text} className={s.messageText}>
+                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
                         {props.message.message.text}
                     </pre>
                 </div>
